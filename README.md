902fca4d2c0aa567b1c870980d17b69ef7ef77df
Colin T.A. Gray <colinta@gmail.com>
2020-05-29 15:18:28 -0400

    refactor parsing, support for rolling 'wild die', better reporting

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 `roll d6`
 
 ```
-d6
---
++---------+
+|  STATS  |
++---------+
+ d6
+----
 min: 1
 max: 6
 avg: 3.5
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+d6
 --
 1: 1 of 6 (16.7%)
 2: 1 of 6 (16.7%)
@@ -13,19 +21,30 @@ avg: 3.5
 4: 1 of 6 (16.7%)
 5: 1 of 6 (16.7%)
 6: 1 of 6 (16.7%)
---
-random roll: 3
+
++----------------+
+|  RANDOM ROLLS  |
++----------------+
+- d6: 1
 ```
 
 `roll 1+2d12`
 
 ```
-1 + 2 × d12
------------
++---------+
+|  STATS  |
++---------+
+ 1 + 2 × d12
+-------------
 min: 3
 max: 25
 avg: 14
------------
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+1+2d12
+------
 3: 1 of 144 (0.7%)
 4: 2 of 144 (1.4%)
 5: 3 of 144 (2.1%)
@@ -49,22 +68,33 @@ avg: 14
 23: 3 of 144 (2.1%)
 24: 2 of 144 (1.4%)
 25: 1 of 144 (0.7%)
------------
-random roll: 22
+
++----------------+
+|  RANDOM ROLLS  |
++----------------+
+- 1 + 2 × d12: 15
 ```
 
 # Target check
 
 ###### 4 or higher succeeds on a d8
 
-`roll d8 4`
+`roll d8/4`
 
 ```
-d8
---
++---------+
+|  STATS  |
++---------+
+ d8
+----
 min: 1
 max: 8
 avg: 4.5
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+d8
 --
 1: 1 of 8 (12.5%)
 2: 1 of 8 (12.5%)
@@ -74,50 +104,76 @@ avg: 4.5
 6: 1 of 8 (12.5%)
 7: 1 of 8 (12.5%)
 8: 1 of 8 (12.5%)
-target >=4: 5 of 8 (62.5%)
---
-random roll: 1
-target: Failed
+
++-----------+
+|  TARGETS  |
++-----------+
+- d8: 5
+%rolls >=4: 5 of 8 (62.5%)
+target: Succeeds
 ```
 
 ###### 4 or LOWER succeeds on a d8
 
-`roll d8 -4`
+`roll d8/-4`
 
 ```
-d8
---
++---------+
+|  STATS  |
++---------+
+ d8
+----
 min: 1
 max: 8
 avg: 4.5
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+d8
 --
 1: 1 of 8 (12.5%)
 2: 1 of 8 (12.5%)
 3: 1 of 8 (12.5%)
 4: 1 of 8 (12.5%)
-target <=4: 4 of 8 (50.0%)
---
-random roll: 2
+5: 1 of 8 (12.5%)
+6: 1 of 8 (12.5%)
+7: 1 of 8 (12.5%)
+8: 1 of 8 (12.5%)
+
++-----------+
+|  TARGETS  |
++-----------+
+- d8: 3
+%rolls <=4: 4 of 8 (50.0%)
 target: Succeeds
 ```
 
 # Chance of either roll succeeding
 
-`roll d8,d6 4`
+`roll d8,d6/+4`
 
 ```
-d8
---
++---------+
+|  STATS  |
++---------+
+ d8
+----
 min: 1
 max: 8
 avg: 4.5
---
-d6
---
+
+ d6
+----
 min: 1
 max: 6
 avg: 3.5
---
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+d8,d6
+-----
 1: 13 of 48 (27.1%)
 2: 13 of 48 (27.1%)
 3: 13 of 48 (27.1%)
@@ -126,42 +182,161 @@ avg: 3.5
 6: 13 of 48 (27.1%)
 7: 6 of 48 (12.5%)
 8: 6 of 48 (12.5%)
-target >=4: 39 of 48 (81.2%)
---
-random rolls:
-d8: 1
-d6: 6
+
++----------------+
+|  TARGET ROLLS  |
++----------------+
+d8,d6
+-----
+- d8: 2
+- d6: 4
+%rolls >=4: 39 of 48 (81.2%)
 target: Succeeds
---
 ```
 
-# Roll multiple target dice in succession, each roll must succeed
+# Roll multiple target dice in succession, every roll must succeed
 
-(weird, I know, but this was a feature I needed)
-
-`roll d8 4 d6 4`
+`roll d8/4 d6/4`
 
 ```
-d8
---
++---------+
+|  STATS  |
++---------+
+ d8
+----
 min: 1
 max: 8
 avg: 4.5
---
-target >=4: 5 of 8 (62.5%)
---
-random roll: 3
-target: Failed
-d6
---
+
+ d6
+----
 min: 1
 max: 6
 avg: 3.5
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+d8
 --
-target >=4: 3 of 6 (50.0%)
+1: 1 of 8 (12.5%)
+2: 1 of 8 (12.5%)
+3: 1 of 8 (12.5%)
+4: 1 of 8 (12.5%)
+5: 1 of 8 (12.5%)
+6: 1 of 8 (12.5%)
+7: 1 of 8 (12.5%)
+8: 1 of 8 (12.5%)
+
+d6
 --
-random roll: 1
+1: 1 of 6 (16.7%)
+2: 1 of 6 (16.7%)
+3: 1 of 6 (16.7%)
+4: 1 of 6 (16.7%)
+5: 1 of 6 (16.7%)
+6: 1 of 6 (16.7%)
+
++----------------+
+|  TARGET ROLLS  |
++----------------+
+- d8: 8
+%rolls >=4: 5 of 8 (62.5%)
+target: Succeeded
+- d6: 1
+%rolls >=4: 3 of 6 (50.0%)
 target: Failed
---------------------
-Total success chance: 31.2%
+
++-----------------+
+|  TOTAL SUCCESS  |
++-----------------+
+%chance: 31.2%
+Target rolls: Failed
+```
+
+# Example
+
+###### Scenario
+
+Player needs to first roll a 4 or higher using a `d10` or a `d6` wild dice,
+_and then_ succeed on a 6 or higher using a `d12` with a `+1` modifier, or a
+`d6` wild dice.
+
+`roll d10,d6/4 1+d12,d6/6`
+
+```
++---------+
+|  STATS  |
++---------+
+ d10
+-----
+min: 1
+max: 10
+avg: 5.5
+
+ d6
+----
+min: 1
+max: 6
+avg: 3.5
+
+ 1 + d12
+---------
+min: 2
+max: 13
+avg: 7.5
+
++---------------+
+|  PERCENTAGES  |
++---------------+
+d10,d6
+------
+1: 15 of 60 (25.0%)
+2: 15 of 60 (25.0%)
+3: 15 of 60 (25.0%)
+4: 15 of 60 (25.0%)
+5: 15 of 60 (25.0%)
+6: 15 of 60 (25.0%)
+7: 6 of 60 (10.0%)
+8: 6 of 60 (10.0%)
+9: 6 of 60 (10.0%)
+10: 6 of 60 (10.0%)
+
+1+d12,d6
+--------
+1: 12 of 72 (16.7%)
+2: 17 of 72 (23.6%)
+3: 17 of 72 (23.6%)
+4: 17 of 72 (23.6%)
+5: 17 of 72 (23.6%)
+6: 17 of 72 (23.6%)
+7: 6 of 72 (8.3%)
+8: 6 of 72 (8.3%)
+9: 6 of 72 (8.3%)
+10: 6 of 72 (8.3%)
+11: 6 of 72 (8.3%)
+12: 6 of 72 (8.3%)
+13: 6 of 72 (8.3%)
+
++----------------+
+|  TARGET ROLLS  |
++----------------+
+d10,d6
+------
+- d10: 6
+- d6: 3
+%rolls >=4: 51 of 60 (85.0%)
+target: Succeeded
+1+d12,d6
+--------
+- 1 + d12: 12
+- d6: 2
+%rolls >=6: 52 of 72 (72.2%)
+target: Succeeded
+
++-----------------+
+|  TOTAL SUCCESS  |
++-----------------+
+%chance: 61.4%
+Target rolls: Succeeded
 ```
--- index.py
+++ index.py
@@ -6,6 +6,40 @@ from functools import reduce
 random.seed()
 
 
+args = sys.argv[1:]
+attempts = []
+options = []
+for arg in args:
+    if arg.startswith('--'):
+        options.append(arg)
+    else:
+        attempts.append(arg)
+if not attempts:
+    sys.stderr.write('Missing dice argument\n')
+    sys.exit(1)
+
+
+re_constant = r'[+-]?\d+'
+re_dice = r'\d* *d\d+'
+re_either = r'({}|{})'.format(re_constant, re_dice)
+re_dice_formula = r'{0}(\+{0})*'.format(re_either)
+re_all_dice = r'{0}(,{0})*'.format(re_dice_formula)
+re_target = r'(/{})?'.format(re_constant)
+re_final = r'^{}{}$'.format(re_all_dice, re_target)
+re_input = re.compile(re_final)
+
+for dice_input in attempts:
+    if not re_input.match(dice_input):
+        sys.stderr.write(f'Invalid dice: `{dice_input}`\n')
+        sys.stderr.write('Dice must be in the form "{{M}}+{{N}}d{{X}}(,...more dice)[/{{T}}]" where M, N, X, T are integers.\n')
+        sys.stderr.write('M - Modifier (positive or negative)\n')
+        sys.stderr.write('N - Number of dice\n')
+        sys.stderr.write('X - Number of sides\n')
+        sys.stderr.write('T - Target value (4 or +4 means "Greater than or equal to 4" and -4 means "Less than or equal to 4")\n')
+        sys.stderr.write('Multiple Dice expressions can be tested against the target value (e.g. Savage Worlds Wild Die).\n')
+        sys.exit(1)
+
+
 class Die:
     def __init__(self, plus, count, die):
         self.plus = plus
@@ -18,15 +52,15 @@ class Die:
     def header(self):
         if self.plus and self.die:
             if self.count == 1:
-                return '{} + d{}'.format(self.plus, self.count, self.die)
+                return f'{self.plus} + d{self.die}'
             else:
-                return '{} + {} × d{}'.format(self.plus, self.count, self.die)
+                return f'{self.plus} + {self.count} × d{self.die}'
         if self.plus:
-            return '{}'.format(self.plus)
+            return f'{self.plus}'
         if self.count == 1:
-            return 'd{}'.format(self.die)
+            return f'd{self.die}'
         else:
-            return '{} × d{}'.format(self.count, self.die)
+            return f'{self.count} × d{self.die}'
 
     def roll(self):
         sum = self.plus
@@ -51,7 +85,7 @@ class Die:
         return one_die_roll(self.die, self.count)
 
     def __repr__(self):
-        return '<{}, min: {}, max: {}, avg: {}>'.format(self.header(), self.min, self.max, self.avg)
+        return f'<{self.header()}, min: {self.min}, max: {self.max}, avg: {self.avg}>'
 
     @classmethod
     def parse(cls, dice_input):
@@ -59,7 +93,7 @@ class Die:
             try:
                 (count, die) = dice_input.split('d', 2)
             except ValueError:
-                sys.stderr.write('Invalid dice: `{}`\n'.format(dice_input))
+                sys.stderr.write(f'Invalid dice: `{dice_input}`\n')
                 sys.exit(1)
             if count:
                 count = int(count)
@@ -68,6 +102,126 @@ class Die:
             return Die(0, count, int(die))
         else:
             return Die(int(dice_input), 0, 0)
+
+
+def section(name):
+    print('+--' + '-' * len(name) + '--+')
+    print(f'|  {name}  |')
+    print('+--' + '-' * len(name) + '--+')
+
+def label(name):
+    print(f' {name}')
+    line(name)
+
+def line(name):
+    if isinstance(name, str):
+        return line(2 + len(name))
+    print('-' * name)
+
+
+class Report:
+    def __init__(self):
+        self.stats = []
+        self.percentages = []
+        self.targets = []
+        self.rolls = []
+        self.successes = None
+
+    def print(self):
+        section('STATS')
+        for (header, dice_min, dice_max, avg) in self.stats:
+            label(header)
+            print(f'min: {dice_min}')
+            print(f'max: {dice_max}')
+            print(f'avg: {avg}')
+            print()
+
+        if self.percentages:
+            section('PERCENTAGES')
+            for data in self.percentages:
+                data.print()
+
+        if self.targets:
+            section('TARGET ROLLS')
+            for target in self.targets:
+                target.print()
+        elif self.rolls:
+            section('RANDOM ROLLS')
+            for roll in self.rolls:
+                roll.print()
+
+        if len(self.successes) > 1:
+            did_succeed, successes = self.successes
+            success = reduce(lambda m, p: m * p, successes, 1)
+            print()
+            section('TOTAL SUCCESS')
+            print(f'%chance: {percent(success, 1)}%')
+            success_text = did_succeed and 'Succeeded' or 'Failed'
+            print(f'Target rolls: {success_text}')
+
+    def append_stats(self, header, dice_min, dice_max, avg):
+        if any(map(lambda stat: stat[0] == header, self.stats)):
+            return
+        self.stats.append((header, dice_min, dice_max, avg))
+
+    def append_percentages(self, data):
+        if any(map(lambda entry: entry.name == data.name, self.percentages)):
+            return
+        self.percentages.append(data)
+
+    def append_target(self, target):
+        if any(map(lambda entry: entry.name == target.name, self.targets)):
+            return
+        self.targets.append(target)
+
+    def append_rolls(self, rolls):
+        self.rolls.append(rolls)
+
+    def set_successes(self, did_succeed, successes):
+        self.successes = (did_succeed, successes)
+
+
+class Percentages:
+    def __init__(self, all_dice):
+        self.name = ','.join(all_dice)
+        self.stats = []
+
+    def print(self):
+        print(self.name)
+        print('-' * len(self.name))
+        for (val, count, len_all, percent) in self.stats:
+            print(f'{val}: {count} of {len_all} ({percent}%)')
+        print()
+
+    def append_stats(self, val, count, len_all, percent):
+        self.stats.append((val, count, len_all, percent))
+
+
+class Target:
+    def __init__(self, all_dice, target, target_gt, target_counts, len_all, percent, all_random_rolls, did_succeed):
+        self.name = ','.join(all_dice)
+        self.stats = (target, target_gt, target_counts, len_all, percent, Rolls(all_random_rolls), did_succeed)
+
+    def print(self):
+        if ',' in self.name:
+            print(self.name)
+            print('-' * len(self.name))
+        (target, target_gt, target_counts, len_all, percent, rolls, did_succeed) = self.stats
+        rolls.print()
+        operator = target_gt and '>=' or '<='
+        print(f'%rolls {operator}{target}: {target_counts} of {len_all} ({percent}%)')
+        success_text = did_succeed and 'Succeeded' or 'Failed'
+        print(f'target: {success_text}')
+
+
+class Rolls:
+    def __init__(self, rolls):
+        self.rolls = rolls
+
+    def print(self):
+        for (header, roll) in self.rolls:
+            print(f'- {header}: {roll}')
+
 
 def add_min(memo, die):
     return memo + die.min
@@ -90,71 +244,50 @@ def percent(val, total):
     return round(1000 * val / float(total)) / 10.0
 
 
-attempts = []
-is_die = True
-attempt_die = None
-for arg in sys.argv[1:]:
-    if is_die:
-        attempt_die = arg
-    else:
-        attempts.append((attempt_die, arg))
-    is_die = not is_die
-if not is_die:
-    attempts.append((attempt_die, None))
-if not attempts:
-    sys.stderr.write('Missing dice argument\n')
-    sys.exit(1)
-
-re_constant = r'\d+'
-re_dice = r'\d*d\d+'
-re_either = r'({}|{})'.format(re_constant, re_dice)
-re_dice_formula = r'{0}(\+{0})*'.format(re_either)
-re_all_dice = r'^{0}(,{0})*$'.format(re_dice_formula)
-re_input = re.compile(re_all_dice)
-
 attempts_did_succeed = True
-attempt_random_rolls = []
 attempt_successes = []
-for (dice_input, target) in attempts:
-    if not re_input.match(dice_input):
-        sys.stderr.write('Invalid dice: `{}`\n'.format(dice_input))
-        sys.stderr.write('Dice must be in the form "[A]+[N]d[X]" where A, N, X are integers.\n')
-        sys.exit(1)
-
-    target_gt = True
-    if target == '-':
-        target = None
-    if target:
-        try:
+report = Report()
+for dice_input in attempts:
+    if '/' in dice_input:
+        dice_input, target = dice_input.split('/', 2)
+        target_gt = True
+        if target == '-':
+            target = None
+        elif target:
             if target.startswith('-'):
                 target_gt = False
                 target = target[1:]
             elif target.startswith('+'):
                 target = target[1:]
 
-            target = int(target)
-        except ValueError:
-            sys.stderr.write('Invalid target: `{}`\n'.format(target))
-            sys.exit(1)
+            try:
+                target = int(target)
+            except ValueError:
+                sys.stderr.write(f'Invalid target: `{target}`\n')
+                sys.exit(1)
+    else:
+        target = None
 
     all_dice_inputs = dice_input.split(',')
+    all_dice = []
     all_possible_rolls = []
+    all_random_rolls = []
     all_min = None
     all_max = None
     all_rolls = []
-    all_headers = []
     any_roll_success = False
     for dice_option in all_dice_inputs:
-        all_dice_inputs = dice_option.split('+')
-        dice = [Die.parse(input) for input in all_dice_inputs]
+        dice_inputs = dice_option.split('+')
+        dice = [Die.parse(input) for input in dice_inputs]
+        all_dice.append(dice)
 
         dice_min = reduce(add_min, dice, 0)
         dice_max = reduce(add_max, dice, 0)
         avg = reduce(add_avg, dice, 0)
         roll = reduce(lambda a, b: a + b, [d.roll() for d in dice], 0)
-        if target_gt:
+        if target and target_gt:
             any_roll_success = any_roll_success or roll >= target
-        else:
+        elif target:
             any_roll_success = any_roll_success or roll <= target
 
         all_rolls.append(roll)
@@ -165,25 +298,19 @@ for (dice_input, target) in attempts:
         all_max = max(dice_max, all_max is None and dice_max or all_max)
 
         header = ' + '.join(map(lambda d: d.header(), dice))
-        attempt_random_rolls.append((header, roll))
-        if not header in all_headers:
-            line = '-' * len(header)
-            print(header)
-            print(line)
-            print('min: {}'.format(dice_min))
-            print('max: {}'.format(dice_max))
-            print('avg: {}'.format(avg))
-            print(line)
-            all_headers.append(header)
+        all_random_rolls.append((header, roll))
+        report.append_stats(header, dice_min, dice_max, avg)
+
     len_all_possible_rolls = len(all_possible_rolls)
 
     # probability of any roll
-    if target:
-        all_attempt_successes = [rolls for rolls in all_possible_rolls if any(map(lambda die_val: die_val >= target, rolls))]
-        for val in range(all_min, all_max + 1):
-            count = len([1 for rolls in all_possible_rolls if any(map(lambda die_val: die_val == val, rolls))])
-            print('{}: {} of {} ({}%)'.format(val, count, len_all_possible_rolls, percent(count, len_all_possible_rolls)))
+    percentages = Percentages(all_dice_inputs)
+    for val in range(all_min, all_max + 1):
+        count = len([1 for rolls in all_possible_rolls if any(map(lambda die_val: die_val == val, rolls))])
+        percentages.append_stats(val, count, len_all_possible_rolls, percent(count, len_all_possible_rolls))
+    report.append_percentages(percentages)
 
+    if target:
         if target_gt:
             target_counts = len([1 for rolls in all_possible_rolls if any(map(lambda die_val: die_val >= target, rolls))])
         else:
@@ -191,16 +318,10 @@ for (dice_input, target) in attempts:
         attempt_successes.append(target_counts / float(len_all_possible_rolls))
         attempts_did_succeed = attempts_did_succeed and any_roll_success
 
-        print('target {}{}: {} of {} ({}%)'.format(target_gt and '>=' or '<=', target, target_counts, len_all_possible_rolls, percent(target_counts, len_all_possible_rolls)))
-        print(line)
-        print('random rolls:')
-        for (header, roll) in attempt_random_rolls:
-            print('{}: {}'.format(header, roll))
-        print('target: {}'.format(any_roll_success and 'Succeeds' or 'Failed'))
-        print(line)
+        target = Target(all_dice_inputs, target, target_gt, target_counts, len_all_possible_rolls, percent(target_counts, len_all_possible_rolls), all_random_rolls, any_roll_success)
+        report.append_target(target)
+    else:
+        report.append_rolls(Rolls(all_random_rolls))
 
-if len(attempt_successes) > 1:
-    success = reduce(lambda m, p: m * p, attempt_successes, 1)
-    print('-' * 20)
-    print('Total success chance: {}%'.format(percent(success, 1)))
-    print('Random rolls: {}'.format(attempts_did_succeed and 'Succeeded' or 'Failed'))
+report.set_successes(attempts_did_succeed, attempt_successes)
+report.print()
